93bb8bfff9eda3c677a509c0cca33b0d28ffc134
Huy0110 <duchuy01102001@gmail.com>
2022-07-19 07:27:54 +0000

    Add save txt and modify confidence scre

--- run_infer.sh
+++ run_infer.sh
@@ -1,4 +1,6 @@
 python tools/infer.py \
---weights /root/YOLOv6/runs/train/exp/weights/16.pt \
+--weights /root/YOLOv6/runs/train/exp/weights/31.pt \
 --source /root/YOLOv6/data_test/images/test \
 --yaml data/dataset.yaml \
+--save-txt \
+--conf-thres 0.6 \
